69859510de847bd4bfb935285afe9d2aba4ba989
neridev <rodrigogregorioneri@hotmail.com>
2019-03-30 16:18:42 -0300

    serviço que permite inserir lista de funcionarios

--- src/main/java/com/rodrigo/neri/stone/service/FuncionarioService.java
+++ src/main/java/com/rodrigo/neri/stone/service/FuncionarioService.java
@@ -1,7 +1,12 @@
 package com.rodrigo.neri.stone.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.rodrigo.neri.stone.model.Funcionario;
 import com.rodrigo.neri.stone.repository.FuncionarioRepository;
@@ -15,6 +20,15 @@ public class FuncionarioService {
 	
 	public Funcionario create(Funcionario funcionario) {
 		return repository.save(funcionario);
+	}
+	
+    
+    public List<Funcionario> createList(@RequestBody List<Funcionario> funcionario) {
+    	 List<Funcionario> result = new ArrayList<>();
+    	 funcionario.forEach(f ->{
+    		 result.add(repository.save(f));
+    	 });
+    	 return result;
 	}
 	
 }
