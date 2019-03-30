342a342c8e76a78ebe0d66cf93587762059aea62
neridev <rodrigogregorioneri@hotmail.com>
2019-03-30 16:43:24 -0300

    metodo bonus responsavel pelo calculo

--- src/main/java/com/rodrigo/neri/stone/service/FuncionarioService.java
+++ src/main/java/com/rodrigo/neri/stone/service/FuncionarioService.java
@@ -5,8 +5,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import com.rodrigo.neri.stone.model.Funcionario;
 import com.rodrigo.neri.stone.repository.FuncionarioRepository;
@@ -23,12 +21,17 @@ public class FuncionarioService {
 	}
 	
     
-    public List<Funcionario> createList(@RequestBody List<Funcionario> funcionario) {
+    public List<Funcionario> createList(List<Funcionario> funcionario) {
     	 List<Funcionario> result = new ArrayList<>();
     	 funcionario.forEach(f ->{
     		 result.add(repository.save(f));
     	 });
     	 return result;
 	}
+    
+    
+    public Double bonus(Double salarioBruto,Double pesoTempoAdmissao, Double pesoAreaAtuacao, Double pesoFaixaSalarial) {
+    	return ((salarioBruto * pesoTempoAdmissao) + (salarioBruto * pesoAreaAtuacao) / pesoFaixaSalarial) * 12 ; 
+    }
 	
 }
