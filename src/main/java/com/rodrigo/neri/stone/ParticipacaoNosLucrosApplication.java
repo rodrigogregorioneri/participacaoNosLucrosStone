e77bc6264c5473e866a541b76feabd4c7ce2a03f
neridev <rodrigogregorioneri@hotmail.com>
2019-03-30 12:57:00 -0300

    subindo projeto inicial já com algumas dependencias necessarias

---
+++ src/main/java/com/rodrigo/neri/stone/ParticipacaoNosLucrosApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigo.neri.stone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParticipacaoNosLucrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParticipacaoNosLucrosApplication.class, args);
+	}
+
+}
