1b9b9b7d92d098f50a48037892337557d1d72981
neridev <rodrigogregorioneri@hotmail.com>
2019-03-31 23:51:30 -0300

    teste do bonus

--- src/test/java/com/rodrigo/neri/stone/service/FuncionarioServiceTest.java
+++ src/test/java/com/rodrigo/neri/stone/service/FuncionarioServiceTest.java
@@ -29,15 +29,15 @@ public class FuncionarioServiceTest {
 		LocalDate ano1 = LocalDate.of(2018, 3, 31);
 		Long peso1 = funcionarioService.getPesoData(ano1);
 		assertTrue(peso1 == 1l);
-		
+
 		LocalDate ano2 = LocalDate.of(2017, 3, 31);
 		Long peso2 = funcionarioService.getPesoData(ano2);
 		assertTrue(peso2 == 2l);
-		
+
 		LocalDate ano3 = LocalDate.of(2014, 3, 31);
 		Long peso3 = funcionarioService.getPesoData(ano3);
 		assertTrue(peso3 == 3l);
-		
+
 		LocalDate ano4 = LocalDate.of(2000, 3, 31);
 		Long peso4 = funcionarioService.getPesoData(ano4);
 		assertTrue(peso4 == 5l);
@@ -47,20 +47,20 @@ public class FuncionarioServiceTest {
 	public void getFaixaSalarialTest() {
 		double qtdSalarios = funcionarioService.getFaixaSalarial(12696.0);
 		assertTrue(qtdSalarios == 5.0);
-		
+
 		double qtdSalarios1 = funcionarioService.getFaixaSalarial(5096.0);
 		assertTrue(qtdSalarios1 == 3.0);
-		
+
 		double qtdSalarios2 = funcionarioService.getFaixaSalarial(3000.0);
 		assertTrue(qtdSalarios2 == 2.0);
-		
+
 		double qtdSalarios3 = funcionarioService.getFaixaSalarial(2696.0);
 		assertTrue(qtdSalarios3 == 1.0);
 	}
-	
+
 	@Test
 	public void validaPesoTest() {
-		Peso p = new Peso(3.0,1.0,5.0);
+		Peso p = new Peso(3.0, 1.0, 5.0);
 		Funcionario func = new Funcionario();
 		func.setArea("Diretoria");
 		func.setCargo("Diretor Financeiro");
@@ -71,12 +71,11 @@ public class FuncionarioServiceTest {
 		Peso peso = funcionarioService.validaPeso(func);
 		assertEquals(peso, peso);
 	}
-	
+
 	@Test
 	public void bonusTest() {
-		
+		double bonus = funcionarioService.bonus(12696.2, 3.0, 1.0, 5.0);
+		assertTrue(bonus == 487534.0800000001);
 	}
-
-
 
 }
